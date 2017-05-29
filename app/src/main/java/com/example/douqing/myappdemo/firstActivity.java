347c9ae58fc1518a4fec80122146c6ff6835c1b1
DQing <15829258737@163.com>
2017-05-29 12:44:21 +0800

    add another layout

---
+++ app/src/main/java/com/example/douqing/myappdemo/firstActivity.java
@@ -0,0 +1,14 @@
+package com.example.douqing.myappdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+    }
+}
