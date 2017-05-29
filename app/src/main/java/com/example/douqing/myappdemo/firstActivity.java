451d13a3c3c8a24931d1120107fa5f2041137727
DQing <15829258737@163.com>
2017-05-29 13:33:00 +0800

    apply menu and intent

--- app/src/main/java/com/example/douqing/myappdemo/firstActivity.java
+++ app/src/main/java/com/example/douqing/myappdemo/firstActivity.java
@@ -1,7 +1,13 @@
 package com.example.douqing.myappdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class firstActivity extends AppCompatActivity {
 
@@ -10,5 +16,34 @@ public class firstActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button1 = (Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(firstActivity.this, MainActivity.class);
+                startActivity(intent);
+//                Toast.makeText(firstActivity.this, "click button1", Toast.LENGTH_SHORT).show();
+//                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "add_item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "remove_item", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
